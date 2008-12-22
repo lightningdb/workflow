d5afe71b8fbcf5b435e35cdb041c8f3ca73c4beb
Dave Bolton <david@davidbolton.net>
2008-12-22 14:45:24 +1100

    now works better with rails 2.2, which is stricter with association proxies and method missing, meaning that respond_to? must be implemented

--- lib/workflow.rb
+++ lib/workflow.rb
@@ -5,7 +5,7 @@ module Workflow
   @@specifications = {}
 
   class << self
-    
+
     def specify(name = :default, meta = {:meta => {}}, &specification)
       if @@specifications[name]
         @@specifications[name].blat(meta[:meta], &specification)
@@ -13,19 +13,19 @@ module Workflow
         @@specifications[name] = Specification.new(meta[:meta], &specification)
       end
     end
-    
+
     def reset!
       @@specifications = {}
     end
-    
+
     def new(name = :default, args = {})
       @@specifications[name].to_instance(args[:reconstitute_at])
     end
-    
+
     def reconstitute(reconstitute_at = nil, name = :default)
       @@specifications[name].to_instance(reconstitute_at)
     end
-    
+
     # this method should be split up into Workflow::Integrator
     # utility sub-classes, with an ActiveRecordIntegrator and a
     # ClassIntegrator... that both respond to integrate! :)
@@ -48,7 +48,7 @@ module Workflow
         receiver.class_eval do
           alias_method :initialize_before_workflow, :initialize
           attr_accessor :workflow
-          
+
           def initialize(attributes = nil)
             initialize_before_workflow(attributes)
             @workflow = Workflow.new(self.class)
@@ -62,7 +62,10 @@ module Workflow
             end
             @workflow.bind_to(self)
           end
-          alias_method :before_save_before_workflow, :before_save # this doesn't work with macro defined callbacks
+          # this aliasing doesn't get called work when a before_save method exists on the model
+          # perhaps the model clobbers the workflow method?
+          # TODO fix this, perhaps with active support style chaining?
+          alias_method :before_save_before_workflow, :before_save
           def before_save
             before_save_before_workflow
             self.workflow_state = @workflow.state.to_s
@@ -86,59 +89,59 @@ module Workflow
         end
       end
     end
-    
+
   end
-  
+
   class Specification
-    
+
     attr_accessor :states, :meta, :on_transition
-    
+
     def initialize(meta = {}, &specification)
       @states = []
       @meta = meta
       instance_eval(&specification)
     end
-    
+
     def to_instance(reconstitute_at = nil)
       Instance.new(states, @on_transition, @meta, reconstitute_at)
     end
-    
+
     def blat(meta = {}, &specification)
       instance_eval(&specification)
     end
-    
+
   private
-  
+
     def state(name, meta = {:meta => {}}, &events_and_etc)
       # meta[:meta] to keep the API consistent..., gah
       self.states << State.new(name, meta[:meta])
       instance_eval(&events_and_etc) if events_and_etc
     end
-    
+
     def on_transition(&proc)
       @on_transition = proc
     end
-    
+
     def event(name, args = {}, &action)
       scoped_state.add_event Event.new(name, args[:transitions_to], (args[:meta] or {}), &action)
     end
-    
+
     def on_entry(&proc)
       scoped_state.on_entry = proc
     end
-    
+
     def on_exit(&proc)
       scoped_state.on_exit = proc
     end
-    
+
     def scoped_state
       states.last
     end
-    
+
   end
-  
+
   class Instance
-    
+
     class TransitionHalted < Exception
 
       attr_reader :halted_because
@@ -149,9 +152,9 @@ module Workflow
       end
 
     end
-    
+
     attr_accessor :states, :meta, :current_state, :on_transition, :context
-    
+
     def initialize(states, on_transition, meta = {}, reconstitute_at = nil)
       @states, @on_transition, @meta = states, on_transition, meta
       @context = self
@@ -161,7 +164,7 @@ module Workflow
         self.current_state = states(reconstitute_at)
       end
     end
-    
+
     def state(fetch = nil)
       if fetch
         states(fetch)
@@ -169,7 +172,7 @@ module Workflow
         current_state.name
       end
     end
-    
+
     def states(name = nil)
       if name
         @states.detect { |s| s.name == name }
@@ -177,14 +180,14 @@ module Workflow
         @states.collect { |s| s.name }
       end
     end
-    
+
     # skips all the hooks, and just updates the state, providing it exists
     def override_state_with(name)
       found = states(name)
       self.current_state = found if found
       self.current_state.name
     end
-        
+
     def method_missing(name, *args)
       if current_state.events(name)
         process_event!(name, *args)
@@ -194,45 +197,58 @@ module Workflow
         super
       end
     end
-    
+
     def bind_to(another_context)
       self.context = another_context
       patch_context(another_context) if another_context != self
     end
-    
+
     def halted?
       @halted
     end
-    
+
     def halted_because
       @halted_because
     end
-    
+
   private
-  
+
     def patch_context(context)
       context.instance_variable_set("@workflow", self)
-      
+
       context.instance_eval do
         alias :method_missing_before_workflow :method_missing
         def method_missing(method, *args)
-          # we create an array of valid method names that can be delegated to the workflow, 
+          # we create an array of valid method names that can be delegated to the workflow,
           # otherwise methods are sent onwards down the chain.
           # this solves issues with catching a NoMethodError when it means something OTHER than a method missing in the workflow instance
-          # for example, perhaps a NoMethodError is raised in an on_entry block or similar. 
-          # "potential_methods" should probably be calculated elsewhere, not per invocation
-          potential_methods = [:states, :state, :override_state_with, :halted?, :halted_because] +
-                            @workflow.states(@workflow.state).events + 
-                            (@workflow.states.collect {|s| "#{s}?".to_sym})
-          if (potential_methods).include?(method.to_sym)
+          # for example, perhaps a NoMethodError is raised in an on_entry block or similar.
+          if potential_methods.include?(method.to_sym)
             @workflow.send(method, *args)
           else
             method_missing_before_workflow(method, *args)
           end
         end
+
+        # rails 2.2 is stricter about method_missing, now we need respond_to
+        alias :respond_to_before_workflow :respond_to?
+        def respond_to?(method)
+          if potential_methods.include?(method.to_sym)
+            return true
+          else
+            respond_to_before_workflow(method)
+          end
+        end
+
+        # TODO "potential_methods" should probably be an instance variable on target context
+        def potential_methods
+          return [:states, :state, :override_state_with, :halted?, :halted_because] +
+                            @workflow.states(@workflow.state).events +
+                            (@workflow.states.collect {|s| "#{s}?".to_sym})
+        end
       end
     end
-    
+
     def process_event!(name, *args)
       event = current_state.events(name)
       @halted_because = nil
@@ -253,55 +269,55 @@ module Workflow
         return_value
       end
     end
-    
+
     def halt(reason = nil)
       @halted_because = reason
       @halted = true
       @raise_exception_on_halt = false
     end
-    
+
     def halt!(reason = nil)
       @halted_because = reason
       @halted = true
       @raise_exception_on_halt = true
     end
-        
+
     def transition(from, to, name, *args)
       run_on_exit(from, to, name, *args)
       self.current_state = to
       run_on_entry(to, from, name, *args)
     end
-    
+
     def run_on_transition(from, to, event, *args)
       context.instance_exec(from.name, to.name, event, *args, &on_transition) if on_transition
     end
-    
+
     def run_action(action, *args)
       context.instance_exec(*args, &action) if action
     end
-    
+
     def run_on_entry(state, prior_state, triggering_event, *args)
       if state.on_entry
         context.instance_exec(prior_state.name, triggering_event, *args, &state.on_entry)
       end
     end
-    
+
     def run_on_exit(state, new_state, triggering_event, *args)
       if state and state.on_exit
         context.instance_exec(new_state.name, triggering_event, *args, &state.on_exit)
       end
     end
-    
+
   end
-  
+
   class State
-    
+
     attr_accessor :name, :events, :meta, :on_entry, :on_exit
-    
+
     def initialize(name, meta = {})
       @name, @events, @meta = name, [], meta
     end
-    
+
     def events(name = nil)
       if name
         @events.detect { |e| e.name == name }
@@ -309,21 +325,21 @@ module Workflow
         @events.collect { |e| e.name }
       end
     end
-    
+
     def add_event(event)
       @events << event
     end
-    
+
   end
-  
+
   class Event
-    
+
     attr_accessor :name, :transitions_to, :meta, :action
-    
+
     def initialize(name, transitions_to, meta = {}, &action)
       @name, @transitions_to, @meta, @action = name, transitions_to, meta, action
     end
-    
+
   end
-  
+
 end
